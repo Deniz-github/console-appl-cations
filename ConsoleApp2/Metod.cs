034cfe3fba86935bf318ccc933a6f496570f0c60
Deniz Aydinoglu <mehmetdenizaydinoglu16@gmail.com>
2021-04-23 21:31:52 +0300

    Yeni git ogrendim

---
+++ ConsoleApp2/Metod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Metod
+    {
+        public static int KareninAlanı(int x)
+        { int alan = x * x;
+            return alan;
+        }
+
+
+
+        public static int DikdörtgeninAlanı(int y, int z)
+        {
+            int alan = y * z;
+            return alan;
+        }
+    }    
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  x;
+            int  y;
+            int  z;
+
+            Console.WriteLine("lütfen bir sayı giriniz");
+
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("alan= {0}", Metod.KareninAlanı(x));
+            Console.WriteLine("Lütfen bir sayı giriniz");
+
+
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Lütfen ikinci sayı giriniz");
+
+
+            z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("alan= {0}", Metod.DikdörtgeninAlanı(y,z));
+        }
+    }
+}
